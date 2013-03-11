0c0e5eb5ffb18e2d95e8869017fd5c37d6a764c0
Rod Nayfield <rod@nayfield.com>
2013-03-11 17:35:46 -0400

    bet utility only for deleting

--- bets.py
+++ bets.py
@@ -5,16 +5,10 @@ import sys
 
 myr=redis.Redis()
 
-if sys.argv[1] == 'add':
-	bettor=sys.argv[2]
-	horse=sys.argv[3]
-	amount=int(sys.argv[4])
-	print myr.hincrby(('derby:bet:'+horse),bettor,amount)
-
-elif sys.argv[1] == 'list':
+if sys.argv[1] == 'list':
 	for horse in myr.keys('derby:bet:*'):
 		for bettor in myr.hkeys(horse):
-			print horse, bettor, myr.hget(horse, bettor)
+			print horse.split(':')[2], "-", bettor, "-", myr.hget(horse, bettor)
 
 elif sys.argv[1] == 'del':
 	bettor=sys.argv[2]
@@ -25,6 +19,5 @@ elif sys.argv[1] == 'del':
 
 else:
 	print "Usage:", sys.argv[0], "add|list|del"
-	print "  add rod secretariat 5"
 	print "  list"
 	print "  del rod secretariat"
